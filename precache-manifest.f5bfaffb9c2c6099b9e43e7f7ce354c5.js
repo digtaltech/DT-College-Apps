250040ede06eb3b332d711ac6160f960f7762a55
DT <kirill.bodunoff@gmail.com>
2020-03-02 03:07:40 +0300

    Updates

---
+++ precache-manifest.f5bfaffb9c2c6099b9e43e7f7ce354c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05bcef74331ee65a1b011c0e1935fbcc",
+    "url": "/DT-College-Apps/index.html"
+  },
+  {
+    "revision": "3280c0f4c16a0204e183",
+    "url": "/DT-College-Apps/static/css/2.ae2f4c93.chunk.css"
+  },
+  {
+    "revision": "19f4039d97219d139224",
+    "url": "/DT-College-Apps/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "3280c0f4c16a0204e183",
+    "url": "/DT-College-Apps/static/js/2.eb430c19.chunk.js"
+  },
+  {
+    "revision": "19f4039d97219d139224",
+    "url": "/DT-College-Apps/static/js/main.13384e3f.chunk.js"
+  },
+  {
+    "revision": "65bbbae26f31ef79cf68",
+    "url": "/DT-College-Apps/static/js/runtime-main.f2b28012.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/DT-College-Apps/static/media/persik.4e1ec840.png"
+  }
+]);
